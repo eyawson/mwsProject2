c6a729e007d5266895c6383bcbf116cede331876
eyawson <ebow.yawson@gmail.com>
2018-09-19 19:03:19 -0400

    initial commit of copied MWS project 1

---
+++ theWorker.js
@@ -0,0 +1,63 @@
+importScripts('js/idb.js');
+
+var staticCacheName = 'precache-v1';
+var dynamicCacheName = 'dynamic-v1';
+
+
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing the bad boy...', event);
+    event.waitUntil(caches.open(staticCacheName).then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/js/dbhelper.js',
+            '/js/idb.js',
+            '/js/main.js',
+            '/index.html',
+            '/restaurant.html',
+            '/css/styles.css',
+            '/img/1.jpg',
+            '/img/2.jpg',
+            '/img/3.jpg',
+            '/img/4.jpg',
+            '/img/5.jpg',
+            '/img/6.jpg',
+            '/img/7.jpg',
+            '/img/8.jpg',
+            '/img/9.jpg',
+            '/img/10.jpg',
+            'https://fonts.googleapis.com/css?family=Lato:400,700,900|Open+Sans:400,700'
+        ]);
+    })
+)
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activated the bad boy...', event);
+    event.waitUntil(caches.keys().then(function(keylist) {
+        return Promise.all(keylist.map(function(key) {
+            if (key !== staticCacheName && key !== dynamicCacheName) {
+                console.log('[Service Worker] Rmoving old cache...', key);
+                return caches.delete(key);
+            }
+        }))
+    }));
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request).then(function(res) {
+                     return caches.open(dynamicCacheName).then(function(cache) {
+                        cache.put(event.request.url, res.clone());
+                        return res;
+                    })
+                }).catch(function(err) {
+
+                });
+            }
+        })
+    )
+});
